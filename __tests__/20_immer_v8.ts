011c74f0946687efd3eec2cebd0d78654722ce72
daishi <daishi@axlight.com>
2021-01-20 18:25:16 +0900

    add failing test

--- __tests__/20_immer_v8.ts
+++ __tests__/20_immer_v8.ts
@@ -6,4 +6,23 @@ const noop = (_arg: unknown) => {
 
 describe('immer v8', () => {
   // See: https://github.com/dai-shi/react-tracked/issues/79
+  it('should work if object is frozen afterward', () => {
+    const proxyCache = new WeakMap();
+    const s1 = { a: { b: 'b' } };
+    const a1 = new WeakMap();
+    const p1 = createDeepProxy(s1, a1, proxyCache);
+    noop(p1.a);
+    expect(isDeepChanged(s1, s1, a1)).toBe(false);
+    expect(isDeepChanged(s1, { a: s1.a }, a1)).toBe(false);
+    expect(isDeepChanged(s1, { a: { b: 'b' } }, a1)).toBe(true);
+    Object.freeze(s1.a);
+    Object.freeze(s1);
+    const a2 = new WeakMap();
+    const p2 = createDeepProxy(s1, a2, proxyCache);
+    noop(p2.a.b);
+    expect(isDeepChanged(s1, s1, a2)).toBe(false);
+    expect(isDeepChanged(s1, { a: s1.a }, a2)).toBe(false);
+    expect(isDeepChanged(s1, { a: { b: 'b' } }, a2)).toBe(false);
+    expect(isDeepChanged(s1, { a: { b: 'b2' } }, a2)).toBe(true);
+  });
 });
