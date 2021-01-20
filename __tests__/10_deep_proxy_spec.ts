7bc94202b37b9a0804f19876fe6b0202581d8165
daishi <daishi@axlight.com>
2021-01-20 12:44:50 +0900

    move a test spec

--- __tests__/10_deep_proxy_spec.ts
+++ __tests__/10_deep_proxy_spec.ts
@@ -217,6 +217,18 @@ describe('special objects spec', () => {
     expect(() => { p1.a = { b: 'b3' }; }).toThrow();
     expect(() => { delete p1.c; }).toThrow();
   });
+
+  it('object with defineProperty (non-configurable and non-writable)', () => {
+    const proxyCache = new WeakMap();
+    const s1: any = {};
+    Object.defineProperty(s1, 'a', { value: { b: 'b' } });
+    const a1 = new WeakMap();
+    const p1 = createDeepProxy(s1, a1, proxyCache);
+    noop(p1.a.b);
+    expect(isDeepChanged(s1, s1, a1)).toBe(false);
+    expect(isDeepChanged(s1, { a: { b: 'b' } }, a1)).toBe(false);
+    expect(isDeepChanged(s1, { a: { b: 'b2' } }, a1)).toBe(true);
+  });
 });
 
 describe('builtin objects spec', () => {
